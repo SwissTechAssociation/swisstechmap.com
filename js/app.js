3c6c9e2aa5183bec547c6a24db0ad20a3a0860eb
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-03-12 12:18:28 +0100

    tests

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation();
+$(document).ready(function() {
+   $(document).foundation({
+      reveal: {
+         animation: 'fade',
+      }
+   });
+   $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+      $('body').addClass('modal-open');
+   });
+   $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+      $('body').removeClass('modal-open');
+   });
+});
