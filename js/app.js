641237533169f90adfd79788c88808ebd9d2df76
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-03-28 10:48:51 +0100

    latest changes - go online

--- js/app.js
+++ js/app.js
@@ -5,12 +5,4 @@ $(document).ready(function() {
          animation: 'fade'
       }
    });
-
-   $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
-      $('body').addClass('modal-open');
-   });
-   $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
-      $('body').removeClass('modal-open');
-   });
-
 });
